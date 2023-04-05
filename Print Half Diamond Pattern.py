219330408acd9967b47d44317d2fedaebedec993
Pragna Yanduri <109524200+Pragna235@users.noreply.github.com>
2023-04-05 21:25:30 +0530

    Add files via upload
    5th April 2023

---
+++ Print Half Diamond Pattern.py
@@ -0,0 +1,10 @@
+n=int(input())
+for i in range(1,n+1):
+    for j in range(i):
+        print('*',end = "")
+    print()
+for i in range(1,n):
+    for j in range(n-i):
+        print('*',end = "")
+    print()
+
